962200e98c52ccbfc50b31dc0c894e79c234d3fe
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2023-09-21 18:29:43 +0530

    franshise signup and login form added

---
+++ src/app/components/franchise-sign-up/franchise-sign-up.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit ,ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'app-franchise-sign-up',
+  templateUrl: './franchise-sign-up.component.html',
+  styleUrls: ['./franchise-sign-up.component.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class FranchiseSignUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
