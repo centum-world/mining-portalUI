fca4404dba8562946e5fd92c1b94de92b9ab1d61
aniimessh <animeshgupta2.002@gmail.com>
2023-09-29 15:19:51 +0530

    login UI created for admin

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,20 +1,19 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Router } from '@angular/router';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
-import { UserService } from 'src/app/service/user.service';
-import { ShareService } from 'src/app/shareService/share.service';
-import { ToastrService } from 'ngx-toastr';
-
-
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { Router } from "@angular/router";
+import { FormGroup, FormControl, FormBuilder } from "@angular/forms";
+import { UserService } from "src/app/service/user.service";
+import { ShareService } from "src/app/shareService/share.service";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
-  encapsulation: ViewEncapsulation.None
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
+  encapsulation: ViewEncapsulation.None,
 })
 export class LoginComponent implements OnInit {
-
+  passwordFieldType: string = "password"; // Initial type is 'password'
+  showPasswordIcon: string = "visibility"; // Initial icon is 'visibility'
 
   loginForm: FormGroup;
   admin_userid: string = "";
@@ -27,15 +26,13 @@ export class LoginComponent implements OnInit {
     private shareService: ShareService,
     private toastr: ToastrService
   ) {
-
     this.loginForm = this.formBuilder.group({
       admin_id: new FormControl(),
       admin_password: new FormControl(),
-    })
-
+    });
   }
   ngOnInit() {
-  //  this.myFunction()
+    //  this.myFunction()
   }
   // myFunction() {
   //   if (!this.functionCalled) {
@@ -43,8 +40,6 @@ export class LoginComponent implements OnInit {
   //     this.functionCalled =true;
   //   }
   // }
-  
-
 
   msa: string = "";
   submit(loginForm) {
@@ -52,40 +47,35 @@ export class LoginComponent implements OnInit {
     this.admin_password = loginForm.controls.admin_password.value;
     let data = {
       user_id: loginForm.controls.admin_id.value,
-      password: loginForm.controls.admin_password.value
-
-
-
-
-    }
+      password: loginForm.controls.admin_password.value,
+    };
     // console.log(data);
     this.userService.login(data).subscribe({
-
-      next: result => {
+      next: (result) => {
         if (result) {
           var data = Object.values(result);
 
           this.shareService.setToken(data[0]);
 
-          this.toastr.success('Login suceessfull', 'success');
-          this.router.navigate(['dashboard']);
+          this.toastr.success("Login suceessfull", "success");
+          this.router.navigate(["dashboard"]);
 
           setTimeout(function () {
             window.location.reload();
           }, 100);
         }
       },
-      error: error => {
-
-        this.toastr.error("Invalid User Id or Password", 'Error');
-        this.router.navigate(['login']);
-
-      }
+      error: (error) => {
+        this.toastr.error("Invalid User Id or Password", "Error");
+        this.router.navigate(["login"]);
+      },
     });
-
   }
 
-
- 
-
+  togglePasswordVisibility(): void {
+    this.passwordFieldType =
+      this.passwordFieldType === "password" ? "text" : "password";
+    this.showPasswordIcon =
+      this.showPasswordIcon === "visibility" ? "visibility_off" : "visibility";
+  }
 }
