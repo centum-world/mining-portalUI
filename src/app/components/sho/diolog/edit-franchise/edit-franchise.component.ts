f95bd0f25cfcbdcd44d9a6aeea3503735ce14fff
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2023-10-03 10:51:24 +0530

    state edit section

--- src/app/components/sho/diolog/edit-franchise/edit-franchise.component.ts
+++ src/app/components/sho/diolog/edit-franchise/edit-franchise.component.ts
@@ -1,53 +1,99 @@
-import { Component, OnInit } from '@angular/core';
-import { Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { FormControl,FormBuilder, FormGroup, Validators,AbstractControl, ValidatorFn } from '@angular/forms';
-import { allState } from 'src/app/components/common/states';
+import { Component, OnInit } from "@angular/core";
+import { Inject } from "@angular/core";
+import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import {
+  FormControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl,
+  ValidatorFn,
+} from "@angular/forms";
+import { allState } from "src/app/components/common/states";
 
 @Component({
-  selector: 'app-edit-franchise',
-  templateUrl: './edit-franchise.component.html',
-  styleUrls: ['./edit-franchise.component.css']
+  selector: "app-edit-franchise",
+  templateUrl: "./edit-franchise.component.html",
+  styleUrls: ["./edit-franchise.component.css"],
 })
 export class EditFranchiseComponent implements OnInit {
-   states = allState.states;
-  selected="Male"
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any,private fb: FormBuilder) {
-    console.log(data)
+  states = allState.states;
+  distric = []
+  franchiseData = {
+    fname: "",
+    lname: "",
+    email: "",
+    phone: "",
+    gender: "",
+    state: "",
+    city: [],
+  };
+  gender: "";
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private fb: FormBuilder
+  ) {
+    this.gender = data.gender;
+    this.franchiseData.fname = data.fname;
+    this.franchiseData.lname = data.lname;
+    this.franchiseData.email = data.email;
+    this.franchiseData.phone = data.phone;
+    this.franchiseData.gender = data.gender;
+    this.franchiseData.state = data.franchiseState;
+    this.franchiseData.city = data.franchiseCity.split(",");
+    const districts =  allState.states.find((state)=>state.state === data.franchiseState)
+    this.distric = districts.districts;
+    console.log(this.franchiseData.city)
   }
-
+  editForm: FormGroup;
   ngOnInit() {
-
+    console.log(this.franchiseData.state);
+    this.editForm = this.fb.group({
+      fname: [this.franchiseData.fname, Validators.required],
+      lname: [this.franchiseData.lname, Validators.required],
+      email: [
+        this.franchiseData.email,
+        [Validators.required, Validators.email],
+      ],
+      phone: [
+        this.franchiseData.phone,
+        [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")],
+      ],
+      gender: [this.franchiseData.gender],
+      state: [this.franchiseData.state],
+      city: [[], Validators.required],
+    });
   }
 
-  
-
-  editForm : FormGroup
-  fname = new FormControl('',[Validators.required]);
-  lname = new FormControl('',[Validators.required]);
-  email = new FormControl('', [Validators.required, Validators.email]);
-  phone = new FormControl('', [
-    Validators.required,
-    Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')
-  ]);
-  gender = new FormControl('', Validators.required);
-  state = new FormControl('',[Validators.required]);
-  city = new FormControl([],Validators.required)
-
-  
+  onStateChange(selectedState: string){
+    this.distric=[];
+    console.log(selectedState)
+    const districts =  allState.states.find((state)=>state.state === selectedState)
+    this.distric = districts.districts;
+ }
 
   getErrorMessage() {
-    return this.email.hasError('required') ? 'You must enter a value' :
-      this.email.hasError('email') ? 'Not a valid email' :
-        '';
+    return this.editForm.get("email").hasError("required")
+      ? "You must enter a value"
+      : this.editForm.get("email").hasError("email")
+      ? "Not a valid email"
+      : "";
   }
-  getErrorFnameMessage(){
-    return this.fname.hasError('required') ? 'You must enter first name':''
+
+  getErrorFnameMessage() {
+    return this.editForm.get("fname").hasError("required")
+      ? "You must enter first name"
+      : "";
   }
-  getErrorLnameMessage(){
-    return this.lname.hasError('required') ? 'You must enter last name':''
+  getErrorLnameMessage() {
+    return this.editForm.get("lname").hasError("required")
+      ? "You must enter last name"
+      : "";
+  }
+
+  editFormSubmit(form: FormGroup){
+    console.log(form.value)
   }
 
   
-
 }
