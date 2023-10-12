a3e8ceb0637748b02478a427eb9fd48650ed45e3
centum-world <centumworldminingrig@gmail.com>
2023-10-12 19:08:11 +0530

    state dashboard responsive

---
+++ src/app/components/modal/partner-payment-details/partner-payment-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-partner-payment-details',
+  templateUrl: './partner-payment-details.component.html',
+  styleUrls: ['./partner-payment-details.component.css']
+})
+export class PartnerPaymentDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
