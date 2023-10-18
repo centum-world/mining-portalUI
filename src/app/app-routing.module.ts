7225b011cfc4226e6087f5e3f9bf5b7438563d39
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2023-10-18 17:55:49 +0530

    partner added in admin side

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -65,6 +65,7 @@ import { FranchiseWithdrawalRequestComponent } from "./franchise-withdrawal-requ
 import { FranchiseWithdrawalSuccessHistoryComponent } from "./franchise-withdrawal-success-history/franchise-withdrawal-success-history.component";
 import { FranchisePartnerMyTeamComponent } from "./franchise-partner-my-team/franchise-partner-my-team.component";
 import { CreateMemberComponent } from "./components/create-member/create-member.component";
+import { CreatePartnerComponent } from "./components/create-partner/create-partner.component";
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
@@ -83,6 +84,7 @@ const routes: Routes = [
     children:[
       {path: "home" , component : HrDashboardComponent},
       {path: "addmember" , component : CreateMemberComponent},
+      {path: "addpartner" , component : CreatePartnerComponent},
       {path: "member-history", component : MemberHistoryComponent},
       {path: "partner-history", component: PartnerHistoryComponent},
       {path: "sho-history", component: ShoHistoryComponent},
