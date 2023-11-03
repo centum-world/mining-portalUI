912626235ff96e3d0863be6b69e8aa2f14a579ff
aniimessh <animeshgupta2.002@gmail.com>
2023-11-03 12:35:17 +0530

    management team developed

---
+++ src/app/components/white-paper/founder-team/founder-team.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-founder-team',
+  templateUrl: './founder-team.component.html',
+  styleUrls: ['./founder-team.component.css']
+})
+export class FounderTeamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
