8eed75378e865c7c267899a4c25ca34db56b69b8
centum-world <centumworldminingrig@gmail.com>
2023-12-12 18:53:34 +0530

    send email in conatct us

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+declare var emailjs: any;
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
+  name: string = '';
+  email: string = '';
+  message: string = '';
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  sendEmail() {
+    const templateParams = {
+      to_name: this.name,
+      from_email: this.email,
+      message: this.message
+    };
 
+    emailjs.send('service_hb3bnbb', 'template_htr5muq', templateParams, 'u1t_nBxroUgPBVoF8')
+      .then((response: any) => {
+        console.log('SUCCESS!', response.status, response.text);
+      }, (err: any) => {
+        console.log('FAILED...', err);
+      });
+  }
 }
