218fdfe9c264de705ee9086c7f06703dd213eb51
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2024-03-01 21:52:43 +0530

    arrow back button showing and working in privacy pollicy

--- src/app/components/common/privacy/privacy.component.ts
+++ src/app/components/common/privacy/privacy.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+import { Location } from '@angular/common';
+
 @Component({
   selector: 'app-privacy',
   templateUrl: './privacy.component.html',
@@ -7,13 +9,17 @@ import { Router } from "@angular/router";
 })
 export class PrivacyComponent implements OnInit {
 
-  constructor(private router : Router) { }
+  constructor(private router: Router, private location: Location) { }
 
   ngOnInit() {
   }
 
-  gotohome(){
-    window.open('https://centumworldrig.com')
+  gotoHome() {
+    window.open('https://centumworldrig.com', '_blank');
+  }
+
+  gotoDashboard() {
+    this.location.back();
   }
 
 }
